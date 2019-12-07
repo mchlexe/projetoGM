fa481800a06ae7a08b6b16400d5923dc20041331
mchlexe <michel.moreira@academico.ifpb.edu.br>
2019-12-06 22:29:18 -0300

    Criação de arquivos de main e biblioteca com comentários do que é necesário desenvolver para primeira entrega.

---
+++ bib.py
@@ -0,0 +1,14 @@
+# Biblioteca de funções que serão utilizadas pelo jogo
+
+# Gerar tabuleiro
+
+# Imprimir tabuleiro
+
+# Jogador 1 e Jogador 2
+
+# Validar jogadas (teste de input)
+
+
+
+
+#Michel Moreira - ADS - IFPB CZ - 2019.2
---
+++ connect4.py
@@ -0,0 +1,14 @@
+# Arquvio main para execução do jogo
+
+#Imprimir regras do jogo
+
+#Gerar e Imprimir o tabuleiro
+
+#Input das jogadas
+
+
+#Como encerrar o jogo (Ganhando ou por pedido do usuário)
+
+
+
+#Michel Moreira - ADS - IFPB CZ - 2019.2
