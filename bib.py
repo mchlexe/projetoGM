0448b3d882c5bb47dcf345dae023825ba9385083
mchlexe <michel.moreira@academico.ifpb.edu.br>
2019-12-09 23:47:20 -0300

    Introdução, regras, gerar tabuleiro e imprimir tabuleiro inseridos.

--- bib.py
+++ bib.py
@@ -1,8 +1,40 @@
 # Biblioteca de funções que serão utilizadas pelo jogo
 
+#Introdução e regras
+def intro():
+  print("""Seja bem vindo ao Connect4!
+
+  >> Objetivo do jogo:
+      > Posicionar quarto preças iguais de forma adjacente formando uma linha, coluna ou diagonal.
+  
+  >> Regras:
+      > O jogo é composto por 2 jogadores (X ou #) que jogam alternadamente.
+      > Cada jogador escolhe uma coluna e a sua peça ocupará a cada mais baixa da mesma.
+      > Se ninguém completar o objetivo, será decretado empate.
+
+  >> Bom jogo!
+  """)
 # Gerar tabuleiro
+def tabuleiro():
+  espaco = ['◯']
+  tabuleiro = []
+
+  for i in range(6):
+    linha = []
+    for linhas in range(7):
+      linha.append(espaco[0])
+    tabuleiro.append(linha)
+  
+  return tabuleiro
 
 # Imprimir tabuleiro
+def imprimirTabuleiro(tabuleiro):
+  print('1', '2', '3', '4', '5', '6', '7')
+
+  for i in range(6):
+    for j in range(7):
+      print(tabuleiro[i][j], end=" ")
+    print()
 
 # Jogador 1 e Jogador 2
 
--- connect4.py
+++ connect4.py
@@ -1,9 +1,14 @@
 # Arquvio main para execução do jogo
 
+import bib
+import os
+
 #Imprimir regras do jogo
+bib.intro()
 
 #Gerar e Imprimir o tabuleiro
-
+tabuleiro = bib.tabuleiro()
+bib.imprimirTabuleiro(tabuleiro)
 #Input das jogadas
 
 
